47636139aa5bdbc267d8cbbe00f63f3de82bd510
Arthur Silva <arthur.silva443@gmail.com>
2024-11-19 14:37:20 -0300

    Padrao Repositorio para Categoria

---
+++ APICatalogo/Repositories/CategoriaRepository.cs
@@ -0,0 +1,54 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using APICatalogo.Repositores;
+
+namespace APICatalogo.Repositories;
+
+public class CategoriaRepository : ICategoriaRepository
+{
+    //Instancia do contenxto
+    private readonly AppDbContext _context;
+
+    public CategoriaRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    //Logica de acesso a dados
+
+    //Get Categorias
+    public IEnumerable<Categoria> GetCategorias()
+    {
+       return _context.Categorias.ToList();
+    }
+
+    //Get Categorias Id
+    public Categoria GetCategoria(int id)
+    {
+        return _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+    }
+
+    //Post Categoria
+    public Categoria Create(Categoria categoria)
+    {
+        if(categoria is null)
+            throw new ArgumentNullException(nameof(categoria));
+
+        _context.Categorias.Add(categoria);
+        _context.SaveChanges();
+
+        return categoria;
+    }
+
+    //Put Categorias
+    public Categoria Update(Categoria categoria)
+    {
+        if (categoria is null)
+            throw new ArgumentNullException(nameof(categoria));
+
+        _context.Entry(categoria).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        _context.SaveChanges();
+
+        return categoria;
+    }
+}
---
+++ APICatalogo/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,12 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.Repositores;
+
+public class ICategoriaRepository
+{
+    IEnumerable<Categoria> GetCategorias();
+    Categoria GetCategoria(int id);
+    Categoria Create(Categoria categoria);
+    Categoria Update(Categoria categoria);
+    Categoria Delete(int id);
+}
