d4d58ee169950eaa281e4d1f3913b309b27c3bf1
Arthur Silva <arthur.silva443@gmail.com>
2024-11-20 15:28:39 -0300

    cricao dos arquivos do unitofwork

---
+++ APICatalogo/Repositories/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using APICatalogo.Repositores;
+
+namespace APICatalogo.Repositories;
+
+public interface IUnitOfWork
+{
+    IProdutoRepository ProdutoRepository { get; }
+    ICategoriaRepository CategoriaRepository { get; }
+    void commit();
+}
