f61f85d65a7579c79ecc719dcec3885fdf615082
Arthur Silva <arthur.silva443@gmail.com>
2024-11-22 21:39:33 -0300

    criacao dos DTOs para usaro autormapping produtos

---
+++ APICatalogo/DTOs/ProdutoDTOMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using APICatalogo.Models;
+using AutoMapper;
+
+namespace APICatalogo.DTOs;
+    public class ProdutoDTOMappingProfile : Profile
+    {
+        public ProdutoDTOMappingProfile()
+        {
+            CreateMap<Produto, ProdutoDTO>().ReverseMap();
+            CreateMap<Categoria, CategoriaDTO>().ReverseMap();
+    }
+    }
+
