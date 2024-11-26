af323310af5896b1fed8678e650f9c4f0219b53b
Arthur Silva <arthur.silva443@gmail.com>
2024-11-26 13:45:45 -0300

    criacao dos arquivos para paginacao do catalago

---
+++ APICatalogo/Extensions/PagedListExtensions.cs
@@ -0,0 +1,21 @@
+﻿using APICatalogo.Models;
+using APICatalogo.DTOs;
+using APICatalogo.DTOs.Mappings;
+using APICatalogo.Pagination; // Aqui você importa as funções de mapeamento.
+
+namespace APICatalogo.Extensions
+{
+    public static class PagedListExtensions
+    {
+        // Método de extensão para converter PagedLitst<Categoria> para IEnumerable<CategoriaDTO>
+        public static IEnumerable<CategoriaDTO> ToCategoriaDtoList(this PagedLitst<Categoria> categorias)
+        {
+            if (categorias == null || !categorias.Any())
+            {
+                return new List<CategoriaDTO>();
+            }
+
+            return categorias.Select(categoria => categoria.ToCategoriaDTO()).ToList();
+        }
+    }
+}
