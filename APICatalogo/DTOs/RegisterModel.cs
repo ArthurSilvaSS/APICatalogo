676b03941a498f35572ab6ac9a86f858a866aaf6
Arthur Silva <arthur.silva443@gmail.com>
2024-11-28 15:14:08 -0300

    criacao dos arquivos dto para implementacao do JWT

---
+++ APICatalogo/DTOs/RegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.DTOs;
+    public class RegisterModel
+    {
+    [Required(ErrorMessage = "User name is required")]
+    public string? UserName { get; set; }
+
+    [EmailAddress]
+    [Required(ErrorMessage = "PassWord is required")]
+    public string? PassWord { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    public string? Email { get; set; }
+}
+
